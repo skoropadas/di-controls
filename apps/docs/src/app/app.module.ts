bbbfbf88e2e130e448d2eab9561287c612885d76
Alexander Skoropad <alex@skoropad.dev>
2023-03-22 21:15:52 +0100

    chore(project): add ng-doc

--- apps/docs/src/app/app.module.ts
+++ apps/docs/src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { NgDocModule } from "@ng-doc/app";
+import { NG_DOC_ROUTING, NgDocGeneratedModule } from "@ng-doc/generated";
+import { RouterModule } from "@angular/router";
+import { NgDocSidebarModule } from "@ng-doc/app/components/sidebar";
+import { NgDocNavbarModule } from "@ng-doc/app/components/navbar";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
@@ -7,7 +13,7 @@ import {ControlDirective} from './control.directive';
 
 @NgModule({
     declarations: [AppComponent, NxWelcomeComponent, ControlDirective],
-    imports: [BrowserModule],
+    imports: [BrowserModule, BrowserAnimationsModule, NgDocNavbarModule, NgDocSidebarModule, RouterModule.forRoot(NG_DOC_ROUTING), NgDocModule.forRoot(), NgDocGeneratedModule.forRoot()],
     providers: [],
     bootstrap: [AppComponent],
 })
