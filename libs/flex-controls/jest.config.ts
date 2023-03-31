28711a3fe2d267febe5321f3f7dd8d02c96f5d1a
Alexander Skoropad <alex@skoropad.dev>
2023-04-01 01:17:14 +0200

    fix(project): angular version

--- libs/flex-controls/jest.config.ts
+++ libs/flex-controls/jest.config.ts
@@ -1,22 +1,23 @@
 /* eslint-disable */
 export default {
-  displayName: "flex-controls",
-  preset: "../../jest.preset.js",
-  setupFilesAfterEnv: ["<rootDir>/test-setup.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-      stringifyContentPathRegex: "\\.(html|svg)$"
-    }
-  },
-  coverageDirectory: "../../coverage/libs/flex-controls",
-  transform: {
-    "^.+\\.(ts|mjs|js|html)$": "jest-preset-angular"
-  },
-  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
-  snapshotSerializers: [
-    "jest-preset-angular/build/serializers/no-ng-attributes",
-    "jest-preset-angular/build/serializers/ng-snapshot",
-    "jest-preset-angular/build/serializers/html-comment"
-  ]
+    displayName: 'flex-controls',
+    preset: '../../jest.preset.js',
+    setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+    globals: {},
+    coverageDirectory: '../../coverage/libs/flex-controls',
+    transform: {
+        '^.+\\.(ts|mjs|js|html)$': [
+            'jest-preset-angular',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+                stringifyContentPathRegex: '\\.(html|svg)$',
+            },
+        ],
+    },
+    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment',
+    ],
 };
