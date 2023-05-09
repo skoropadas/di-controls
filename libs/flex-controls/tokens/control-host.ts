6bf3b444873e95d52f152acb463e0a0ccfa5644b
Alex Skoropad <alex@skoropad.dev>
2023-05-09 15:14:16 +0200

    fix(project): remove `forwardRef` for `provideControlHost`

--- libs/flex-controls/tokens/control-host.ts
+++ libs/flex-controls/tokens/control-host.ts
@@ -1,4 +1,4 @@
-import {forwardRef, InjectionToken, Provider, Type} from '@angular/core';
+import {InjectionToken, Provider, Type} from '@angular/core';
 import {FlControlHost} from 'flex-controls/controls';
 import {ComponentType} from 'flex-controls/types';
 
@@ -6,9 +6,9 @@ export const FL_CONTROL_HOST: InjectionToken<ComponentType<FlControlHost<unknown
     ComponentType<FlControlHost<unknown>>
 >('FL_CONTROL_HOST');
 
-export function provideControlHost<T extends FlControlHost<unknown>>(componentOrDirective: Type<T>): Provider {
+export function provideControlHost<T extends FlControlHost<unknown>>(useExisting: Type<T>): Provider {
     return {
         provide: FL_CONTROL_HOST,
-        useExisting: forwardRef(() => componentOrDirective),
+        useExisting,
     };
 }
