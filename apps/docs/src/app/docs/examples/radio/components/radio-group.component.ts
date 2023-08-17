ac2cf0537e9846572ef21d96e661431304af8f6f
Filipp Chibisov <fil.chibisov.2000@mail.ru>
2023-08-17 16:47:42 +0300

    docs(radio) add docs for radio button (#1)

---
+++ apps/docs/src/app/docs/examples/radio/components/radio-group.component.ts
@@ -0,0 +1,26 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DIControl, provideHostControl} from 'di-controls';
+
+@Component({
+	selector: 'di-radio-group',
+	standalone: true,
+	imports: [CommonModule],
+	template: `<ng-content></ng-content>`,
+	styles: [
+		`
+			:host {
+				display: flex;
+				flex-direction: column;
+				gap: 8px;
+			}
+		`,
+	],
+	providers: [provideHostControl(RadioGroupComponent)],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RadioGroupComponent<T = unknown> extends DIControl<T> {
+	constructor() {
+		super();
+	}
+}
