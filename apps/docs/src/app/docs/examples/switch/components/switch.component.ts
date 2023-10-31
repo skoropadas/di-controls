7271652e92b5a4df9768bb708e167b05b16aa4c3
Alex Skoropad <alex@skoropad.dev>
2023-10-31 17:41:30 +0100

    refactor(project): refactor hooks and configuration API

--- apps/docs/src/app/docs/examples/switch/components/switch.component.ts
+++ apps/docs/src/app/docs/examples/switch/components/switch.component.ts
@@ -1,60 +1,62 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {DI_HOST_CONTROL, DICompareHost, DIStateControl} from 'di-controls';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DICompareHost, DIStateControl, injectHostControl } from 'di-controls';
 
 @Component({
 	standalone: true,
 	selector: 'di-switch',
 	template: `
-		<button
-			type="button"
-			(click)="toggle(); touch()">
+		<button type="button" (click)="toggle(); touch()">
 			<span [class.checked]="checked()"></span>
 		</button>
-		<ng-content/>
+		<ng-content />
 	`,
-	styles: [`
-		:host {
-			position: relative;
-			display: flex;
-			align-items: center;
-			gap: 8px;
-
-			& > button {
+	styles: [
+		`
+			:host {
+				position: relative;
 				display: flex;
-				flex-flow: row nowrap;
 				align-items: center;
-				height: 20px;
-				width: 40px;
-				padding: 2px;
-				border: none;
-				border-radius: 100px;
+				gap: 8px;
 
-				& > span {
-					position: relative;
-					display: inline-block;
-					vertical-align: middle;
-					width: 16px;
-					height: 16px;
-					background-color: #a40000;
-					cursor: pointer;
+				& > button {
+					display: flex;
+					flex-flow: row nowrap;
+					align-items: center;
+					height: 20px;
+					width: 40px;
+					padding: 2px;
+					border: none;
 					border-radius: 100px;
-					transition: .25s;
 
-					&.checked {
-						margin-left: 20px;
-						background-color: #14c814;
+					& > span {
+						position: relative;
+						display: inline-block;
+						vertical-align: middle;
+						width: 16px;
+						height: 16px;
+						background-color: #a40000;
+						cursor: pointer;
+						border-radius: 100px;
+						transition: 0.25s;
+
+						&.checked {
+							margin-left: 20px;
+							background-color: #14c814;
+						}
 					}
 				}
 			}
-		}
-	`],
+		`,
+	],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [CommonModule]
+	imports: [CommonModule],
 })
 export class SwitchComponent<T = unknown> extends DIStateControl<T> {
-
 	constructor() {
-		super(inject(DI_HOST_CONTROL, {optional: true}), inject(DICompareHost, {optional: true}), false);
+		super({
+			host: injectHostControl({ optional: true }),
+			compareHost: inject(DICompareHost, { optional: true }),
+		});
 	}
 }
